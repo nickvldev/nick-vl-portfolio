a92dee972fb35a56796c203115bdc0d98f36e31f
Nick Van Looveren <nick.vanlooveren@rombit.be>
2026-04-09 21:16:39 +0200

    improved aspect ratio for hero image

--- src/components/about/intro.tsx
+++ src/components/about/intro.tsx
@@ -12,18 +12,16 @@ export function Intro({ intro }: IntroProps) {
   const { title, subtitle, introText } = intro;
 
   return (
-    <section className="pt-6">
-      <div className="relative aspect-4/3 sm:aspect-16/10 lg:aspect-video max-h-[85svh] max-w-360 mx-auto overflow-hidden rounded-4xl">
-        <Image
-          src="/images/about-me-hero.png"
-          alt="About me hero"
-          quality={90}
-          fill
-          className="object-cover grayscale opacity-50 brightness-110"
-          priority
-        />
-        <div className="absolute inset-0 bg-teal-600/35 mix-blend-color" />
-      </div>
+    <section className="relative aspect-4/3 sm:aspect-3/2 lg:aspect-2/1 2xl:aspect-21/9 min-[160rem]:max-h-[85svh] max-w-640 mx-auto overflow-hidden">
+      <Image
+        src="/images/about-me-hero.png"
+        alt="About me hero"
+        quality={90}
+        fill
+        className="object-cover grayscale opacity-50 brightness-110"
+        priority
+      />
+      <div className="absolute inset-0 bg-teal-600/35 mix-blend-color" />
     </section>
   );
 }
